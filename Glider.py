45c64dcc44fadf10ee81bab96d171434c8432314
Andreas Krietemeyer <akrietemeyer@socib.es>
2016-08-11 09:31:40 +0200

    Basic read-in implemented.

--- Glider.py
+++ Glider.py
@@ -1,6 +1,54 @@
+import json
+import numpy as np
 from glider_utils import *
+from netCDF4 import Dataset
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('glider.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s p%(process)s {%(pathname)s:%(lineno)d} - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 class Glider:
     def __init__(self):
+        self.root = None
+        self.time = []
+        self.depth = []
+        self.config = dict()
+        self.general_section = 'General'
+        self.scientific_section = 'Scientifical'
+        self.variable_data_interest = dict()
+        self.read_config()
+        self.read_netcdf_file()
+        create_folder(self.config[self.general_section]['out_dir'] + self.config[self.general_section]['name'])
         pass
+
+    def read_config(self):
+        self.config[self.general_section] = {
+            'name': read_value_config(self.general_section, 'project_name'),
+            'link': read_value_config(self.general_section, 'link'),
+            'out_dir': read_value_config(self.general_section, 'output_directory')
+        }
+        self.config[self.scientific_section] = {
+            't_s_range': read_value_config(self.scientific_section, 'temperature_salinity_range'),
+            'full_prof_vars': map(str, json.loads(read_value_config(self.scientific_section, 'full_profiles_variables'))),
+            'prof_vars': map(str, json.loads(read_value_config(self.scientific_section, 'profile_viewer_variables')))
+        }
+
+    def read_netcdf_file(self):
+        try:
+            logger.info('Reading dataset...')
+            self.root = Dataset(self.config[self.general_section]['link'])
+        except RuntimeError:
+            logger.error('Cannot read dataset {0}.'.format(self.config[self.general_section]['link']))
+        self.time = get_data_array(self.root['time'])
+        self.depth = get_data_array(self.root['depth'])
+        read_variables = np.union1d(self.config['Scientifical']['full_prof_vars'],
+                                    self.config['Scientifical']['prof_vars'])
+        logger.info('Read variables of interest...')
+        for cur_var in read_variables:
+            self.variable_data_interest[cur_var] = get_data_array(self.root[cur_var])
+
--- glider_utils.py
+++ glider_utils.py
@@ -1,4 +1,67 @@
 import numpy as np
+import matplotlib.cm as cm
+from matplotlib import colors as mp_colors
+import ConfigParser
+import os
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('utils.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s p%(process)s {%(pathname)s:%(lineno)d} - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def get_data_color_palette(data, colormap_name):
+    colormap = cm.get_cmap(colormap_name)
+    bokeh_palette = [mp_colors.rgb2hex(m) for m in colormap(np.arange(colormap.N))]
+    data_min = np.nanmin(data)
+    data_max = np.nanmax(data)
+    abs_range = abs(data_min) + abs(data_max)
+    abs_range /= 256.0
+
+    color_list = np.zeros(shape=(len(data), 1), dtype=object)
+    counter = 0
+    for _ in data:
+        color_list[counter] = ""
+        counter += 1
+
+    counter = 1
+    lower_limit = data_min
+    for i in bokeh_palette:
+        upper_limit = data_min + abs_range*counter
+        test = np.where((data >= lower_limit) & (data <= upper_limit))
+        color_list[test] = i
+        counter += 1
+        lower_limit = upper_limit
+    return color_list
+
+
+def plot_temperature_salinity_diagram():
+    pass
+
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def read_value_config(section, variable):
+    config_handler = ConfigParser.ConfigParser()
+    config_handler.read(os.getcwd() + '/config.ini')
+    if config_handler.has_section(section):
+        return config_handler.get(section, variable)
+    else:
+        logger.warning('Specified section ' + section + ' not found.')
+        return ''
+
+
+def is_l2_processing_level(link):
+    if link.find('/L2/'):
+        return True
+    else:
+        return False
 
 
 def get_data_array(data_array):
